2f6897b98c343eab99f2f86626690c8647d6dd0e
Kazooooo <cest93@gmail.com>
2017-02-26 00:31:02 +0900

    learning complete

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,14 +1,23 @@
-import { renderComponent , expect } from '../test_helper';
+import { renderComponent, expect } from '../test_helper';
 import App from '../../src/components/app';
 
-describe('App' , () => {
+// Use 'describe' to group together similar tests
+describe('App', () => {
+  // initialize a component variable
   let component;
 
   beforeEach(() => {
+    // substitute an instance of App every single time before running a test
     component = renderComponent(App);
   });
 
-  it('renders something', () => {
-    expect(component).to.exist;
+  // Use 'it' to test a single attribute of a target
+  it('shows a comment box', () => {
+    // Use 'expect' to make an 'assertion' about a target
+    expect(component.find('.comment-box')).to.exist;
+  });
+
+  it('shows a comment list', () => {
+    expect(component.find('.comment-list')).to.exist;
   });
 });
